c00bb4f6388fbbc9e054484be2743487ab283d60
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2024-02-19 14:48:34 +0200

    Update galactic_empire_maker.py

---
+++ galactic_empire_maker/galactic_empire_maker.py
@@ -0,0 +1,59 @@
+import random
+
+
+class Delegation:
+    def __init__(
+        self,
+        name,
+        materials,
+        suggested,
+    ):
+        self.name = name
+        self.materials = materials
+        self.suggested = suggested
+
+    def get_suggested(self):
+        return self.suggested
+
+    def create_suggestion(self, length):
+        suggestion_list = []
+        for i in range(length):
+            random_suggestion = random.randint(0, len(self.materials) - 1)
+            suggestion_list.append(self.materials[random_suggestion])
+        return suggestion_list
+
+    def negotiation(self, alien_delegation):
+        if isinstance(alien_delegation, Delegation):
+            alien_suggested = alien_delegation.get_suggested()
+            suggestion_list = self.create_suggestion(len(alien_suggested))
+            for needed_material in alien_suggested:
+                if needed_material in suggestion_list:
+                    return True
+        return False
+
+
+humen_material = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+]
+humen_delegation = Delegation("Humen", humen_material, humen_material)
+klingon_delegation = Delegation("Klingon", ["A", "B", "R"], ["A", "B", "R"])
+
+print(humen_delegation.negotiation(klingon_delegation))
