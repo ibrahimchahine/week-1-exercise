fdd3415a9aee62a2440d6b7c188498a3db473548
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2024-02-19 10:41:36 +0200

    Create pokemon_battle_simulator.py

---
+++ pokemon_battle_simulator/pokemon_battle_simulator.py
@@ -0,0 +1,17 @@
+class Pokemon:
+    def __init__(self, name, level, strength, speed, type):
+        self.name = name
+        self.level = level
+        self.strength = strength
+        self.speed = speed
+        self.type = type
+        self.life = 120
+
+    def update_score_speed(self, num):
+        self.speed += num
+
+
+class Player:
+    def __init__(self, name, pokemons):
+        self.name = name
+        self.pokemons = pokemons
