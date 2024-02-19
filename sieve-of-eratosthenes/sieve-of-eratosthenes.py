0ad46252ed6420f25883f55f91edf781c8665c6f
Ibrahem Chahine <22155702+ibrahimchahine@users.noreply.github.com>
2024-02-19 13:32:03 +0200

    Update sieve-of-eratosthenes.py

--- sieve-of-eratosthenes/sieve-of-eratosthenes.py
+++ sieve-of-eratosthenes/sieve-of-eratosthenes.py
@@ -1,6 +1,22 @@
 def sieve_of_eratosthenes(number_list):
-    return
+    prime_numbers = [2, 3]
+    array = list(number_list)
+    length = len(array)
+    i = 0
+    while i < length:
+        num = array[i]
+        for prime in prime_numbers:
+            if num % prime == 0:
+                i += 1
+                break
+            prime_numbers.append(num)
+            array.pop(i)
+            break
+        length = len(array)
+    return prime_numbers
 
 
-max = int(input("Enter your maximum number"))
-sieve_of_eratosthenes()
+max = int(input("Enter The maximum number (N>3)"))
+array = [i for i in range(4, max)]
+primes = sieve_of_eratosthenes(array)
+print(primes, len(primes))
